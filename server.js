f7ea36102d6d62cb4d56479735e0985db6e00c9f
unknown <mikeverasaragi@gmail.com>
2019-05-10 15:42:42 +0700

    updates heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app-testangular'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/app-testangular/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
